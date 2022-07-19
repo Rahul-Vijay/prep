f1abb89f4ed33d49b30b6c0cf111647bcaea9749
Rahul-Vijay <vk.rahul318@gmail.com>
2022-07-19 15:08:38 +0530

    letter combinations

---
+++ backtracking/letter_combinations.js
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+
+const KEYBOARD = {
+  2: "abc",
+  3: "def",
+  4: "ghi",
+  5: "jkl",
+  6: "mno",
+  7: "pqrs",
+  8: "tuv",
+  9: "wxyz",
+};
+
+var dfs = function (path, digits, res) {
+  if (path.length === digits.length) {
+    res.push(path.join(""));
+    return;
+  }
+  let next_number = digits.charAt(path.length);
+  for (let letter of KEYBOARD[next_number]) {
+    path.push(letter);
+    dfs(path, digits, res);
+    path.pop();
+  }
+};
+
+var letterCombinations = function (digits) {
+  const res = [];
+  dfs([], digits, res);
+  return res;
+};
