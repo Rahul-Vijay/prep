6e5a46276208e33950382cbc7c254307f49266d0
Rahul-Vijay <vk.rahul318@gmail.com>
2022-07-22 19:12:16 +0530

    bfs & dfs on a graph

---
+++ 6_Graphs/0_BFS&DFS_on_graph.js
@@ -0,0 +1,20 @@
+function bfs(root) {
+  let q = [root];
+  let visited = new Set([root]);
+  while (q.length > 0) {
+    const node = q.shift();
+    for (const neighbour of get_neighbours(node)) {
+      if (visited.has(neighbour)) continue;
+      q.push(neighbour);
+      visited.push(neighbour);
+    }
+  }
+}
+
+function dfs(visited, root) {
+  for (const neighbour of get_neighbours(root)) {
+    if (visited.has(neighbour)) continue;
+    visited.add(neighbour);
+    dfs(neighbour, visited);
+  }
+}
